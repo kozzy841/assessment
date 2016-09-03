97dd55906fb6c05cffa7ee1a832cf605fdd2ca92
kozzy841 <sanpo.kyoto@gmail.com>
2016-09-03 22:13:43 +0900

    #1に対するへんこう

--- assessment.js
+++ assessment.js
@@ -76,20 +76,20 @@
     * @param {string} userName ユーザーの名前
     * @return {string} 診断結果
     */
-    function assessment(userName) {
-        // 全文字のコード番号を取得してそれを足し合わせる
-        let sumOfcharCode = 0;
-        for (let i = 0; i < userName.length; i++) {
-            sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
-        }
+    // function assessment(userName) {
+    //     // 全文字のコード番号を取得してそれを足し合わせる
+    //     let sumOfcharCode = 0;
+    //     for (let i = 0; i < userName.length; i++) {
+    //         sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+    //     }
 
-        // 文字のコード番号の合計を回答の数で割って添字の数値を求める
-        const index = sumOfcharCode % answers.length;
-        let result = answers[index];
+    //     // 文字のコード番号の合計を回答の数で割って添字の数値を求める
+    //     const index = sumOfcharCode % answers.length;
+    //     let result = answers[index];
 
-        result = result.replace(/{userName}/g, userName);
-        return result;
-    }
+    //     result = result.replace(/{userName}/g, userName);
+    //     return result;
+    // }
 
     // テストコード
     console.assert(
